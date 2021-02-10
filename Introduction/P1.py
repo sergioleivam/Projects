0929fc7946ce4d15e43f6931b1a3371a5680b8b2
Sergio Leiva Montecinos <sergioleivamontecinos@Sergios-MacBook-Air.local>
2021-02-10 17:46:55 -0300

    Part_1

--- Introduction/P1.py
+++ Introduction/P1.py
@@ -1,1 +1,8 @@
+###############################
+########   Part 1    ##########
+###############################
+
+# Star by downloading and installing python into your computer
+
 print('Hello world')
+
