7b8ace47bc9536c65f3c5e586457a2065299dea4
Sergio Leiva Montecinos <sergioleivamontecinos@Sergios-MacBook-Air.local>
2021-02-10 17:47:11 -0300

    Part_2

---
+++ Introduction/P2.py
@@ -0,0 +1,20 @@
+###############################
+########   Part 2    ##########
+###############################
+
+# Creating a tuple
+programming_languages = "Python", "Java", "C++", "C#"
+
+#Print the type of the new variable: programming__languages
+print(type(programming_languages))
+
+# Creating a tuple, Alternative way
+programming_languages = ("Python", "Java", "C++", "C#")
+
+# Print each item of programming_languages by iteration: for
+
+for language in programming_languages:
+    # Common indentation is 4 spaces, although in google they use 2. 
+    print(language)
+
+
