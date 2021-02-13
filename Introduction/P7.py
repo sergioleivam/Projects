20bc382d99f4e13da78385eae5ffd56b859fbc65
Sergio Leiva Montecinos <sergioleivamontecinos@Sergios-MacBook-Air.local>
2021-02-13 17:39:39 -0300

    Add some tests with just_display

--- Introduction/P7.py
+++ Introduction/P7.py
@@ -60,3 +60,7 @@ def game_board(player=0, row=0, column=0, just_display=False):
     game[row][column] = player
     for count_en, row in enumerate(game):
         print(count_en,row)
+
+# Some tests 
+game_board(just_display=True)
+game_board(player=1, row=2, column=1)
