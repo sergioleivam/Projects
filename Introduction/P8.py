72f7aa262301282f39c68c45c439b9ac637ff571
Sergio Leiva Montecinos <sergioleivamontecinos@Sergios-MacBook-Air.local>
2021-02-14 19:39:16 -0300

    correction as comment inside function

--- Introduction/P8.py
+++ Introduction/P8.py
@@ -35,6 +35,7 @@ game = "I want to play a game"
 
 def game_board(player=0, row=0, column=0, just_display=False):
     game = "A game"
+    # This modification wont work, because it change a "local" variable inside the function
 
 print(game)
 game_board()
