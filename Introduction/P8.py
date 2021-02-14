ee4d53a9f159ab3f78ede3ca6635fb08a564224a
Sergio Leiva Montecinos <sergioleivamontecinos@Sergios-MacBook-Air.local>
2021-02-14 19:40:55 -0300

    pseudo correction with print inside function

--- Introduction/P8.py
+++ Introduction/P8.py
@@ -36,7 +36,10 @@ game = "I want to play a game"
 def game_board(player=0, row=0, column=0, just_display=False):
     game = "A game"
     # This modification wont work, because it change a "local" variable inside the function
-
+    # However, if we print game inside the function as:
+    print(game)
+    # We see that it "work"
+    
 print(game)
 game_board()
 print(game)
