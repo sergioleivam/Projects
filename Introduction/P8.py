e7ccc2b683c17dbc22bc510d83e39d360e903e45
Sergio Leiva Montecinos <sergioleivamontecinos@Sergios-MacBook-Air.local>
2021-02-14 19:09:15 -0300

    missed definition game

--- Introduction/P8.py
+++ Introduction/P8.py
@@ -1,10 +1,11 @@
+game = [[0,0,0],
+        [0,0,0],
+        [0,0,0],]
+
 def game_board(player=0, row=0, column=0, just_display=False):
     print("  a  b  c")
     if not just_display:
-        #It works when just_display is false
         game[row][column] = player
-        # Therefore, if just_display = True we do not make any changes
-    game[row][column] = player
     for count_en, row in enumerate(game):
         print(count_en,row)
 
