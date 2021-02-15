153cdc6094ff108c0a5df6664855d2b4b880c131
Sergio Leiva Montecinos <sergioleivamontecinos@Sergios-MacBook-Air.local>
2021-02-15 17:47:48 -0300

    first win horizontaly

--- Introduction/P10.py
+++ Introduction/P10.py
@@ -14,3 +14,20 @@ game = [[1,1,1],
 # 1) Horizontally
 # 2) Vertical
 # 3) Diagonal
+
+#1. Horizontally
+# A naive way would be compare the first element of each column of the row with the rest of the row, 
+# but that can get very messy very quickly
+# So, another way is:
+
+def win(current_game):
+    for row in game:
+        print(row)
+        col1 = row[0]
+        col2 = row[1]
+        col3 = row[2]
+        
+        if col1 == col2 == col3:
+            print("winner !!!")
+
+win(game)
