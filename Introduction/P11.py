1afd9a8d6a0def32bc7aa79c2c3693deee8cc5e2
Sergio Leiva Montecinos <sergioleivamontecinos@Sergios-MacBook-Air.local>
2021-02-15 18:16:22 -0300

    First solution

--- Introduction/P11.py
+++ Introduction/P11.py
@@ -19,5 +19,27 @@ game = [[2,0,1],
 
 # To check this kind of win, we need to check the same index in every row to be the same. Like:
 
+check = []     # list to check
+
+# for row in game:
+#     check.append(row[0]) # We add the element of the row to the check-list.
+
+# # Re using the structure of the horizontal win:
+
+# if check.count(check[0]) == len(check) and check[0] != 0:
+#     print("Winner!")
+
+# Lets check if we dont win
+game = [[2,0,1],
+        [0,0,0],
+        [2,2,0],]
+
+check = []     # list to check
+
 for row in game:
-    print(row[0])
+    check.append(row[0]) # We add the element of the row to the check-list.
+
+# Re using the structure of the horizontal win:
+
+if check.count(check[0]) == len(check) and check[0] != 0:
+    print("Winner!")
