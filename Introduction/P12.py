828fc70da7a49f94d5a4d74bdb8988a7f4ad9a1f
Sergio Leiva Montecinos <sergioleivamontecinos@Sergios-MacBook-Air.local>
2021-02-17 15:59:08 -0300

    hard coding check diagonal win

--- Introduction/P12.py
+++ Introduction/P12.py
@@ -16,3 +16,19 @@ game = [[2,0,1],
 # 3) Diagonal
 
 # 3) Diagonal 
+
+# There are two ways we can win diagonally, and we want to keep it scaleable
+
+game = [[2,0,1],
+        [2,2,0],
+        [2,2,2],]
+
+# First way, the index has to increase at the same time, [0,0], [0+1,0+1], [1+1,1+1]
+# Second way, one index increase and the other decrease by the same amount, [2,0], [2-1,0+1], [1-1,1+1]
+
+# Lets check, hard coding it
+
+if game[0][0] == game[1][1] == game[2][2]:
+        print("Winner")
+if game[2][0] == game[1][1] == game[0][2]:
+        print("Winne")
