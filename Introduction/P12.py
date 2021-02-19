51fbebcfd14959cabc0f01f90d80797b08a6bccb
Sergio Leiva Montecinos <sergioleivamontecinos@Sergios-MacBook-Air.local>
2021-02-19 19:27:19 -0300

    Add a second no-winner case

--- Introduction/P12.py
+++ Introduction/P12.py
@@ -23,8 +23,8 @@ game = [[2,0,1],
         [2,2,0],
         [2,2,2],]
 
-# First way, the index has to increase at the same time, [0,0], [0+1,0+1], [1+1,1+1]
-# Second way, one index increase and the other decrease by the same amount, [2,0], [2-1,0+1], [1-1,1+1]
+# First way, the index has to increase at the same time, [0][0], [0+1][0+1], [1+1][1+1]
+# Second way, one index increase and the other decrease by the same amount, [2][0], [2-1][0+1], [1-1][1+1]
 
 # Lets check, hard coding it
 
@@ -42,3 +42,18 @@ diags = []
 for ix in range(len(game)):
         diags.append(game[ix][ix])
 print(diags)
+
+
+# Second try, with no winner
+game = [[2,0,1],
+        [2,2,0],
+        [2,2,1],]
+
+# Star by creating a list
+diags = []
+# Fill the diagonal
+for ix in range(len(game)):
+        diags.append(game[ix][ix])
+print(diags)
+
+# Here we can repeat the code for the vertical win
