befce89425c3898328d51aa77f7285eb4b121a9e
Sergio Leiva Montecinos <sergioleivamontecinos@Sergios-MacBook-Air.local>
2021-02-24 19:38:19 -0300

    first functions

---
+++ Data Analysis/P01.py
@@ -0,0 +1,9 @@
+# In this series we will review the basics of panda and play with some sort of 
+# Pandas is a library that allow us to work with data that can be order or study in rows and columns
+# search in google: pandas python and in pandas.pydata you can find more about it.
+
+# In the tutorial they use pandas 0.24.1, we have 1.0.5
+# We are going to use data set from kaggle, no affiliation just a cool place to get data sets
+# To use kaggle we need to create an acounts, mine is sergio leiva or sergioleiva1
+
+##############################
